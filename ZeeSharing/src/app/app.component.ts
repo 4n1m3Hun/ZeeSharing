adc964e18eafaa58b67a568733cc04adebfc83cc
4n1m3Hun <tomi2001.tst@gmail.com>
2024-10-01 16:33:22 +0200

    adat lekérés

--- ZeeSharing/src/app/app.component.ts
+++ ZeeSharing/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ZeeSharing';
+  item$: Observable<Item[]>;
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'Musics');
+    this.item$ = collectionData<Item>(itemCollection);
+  }
+}
+interface Item {
+  name: string,
 }
--- ZeeSharing/src/app/app.config.ts
+++ ZeeSharing/src/app/app.config.ts
@@ -4,6 +4,24 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideFirebaseApp(() => initializeApp({
+
+    apiKey: "AIzaSyD0YhsRRNmsdIon2QEAff_L3Ixqm4ckmyk",
+  
+    authDomain: "zeesharing-d33f2.firebaseapp.com",
+  
+    projectId: "zeesharing-d33f2",
+  
+    storageBucket: "zeesharing-d33f2.appspot.com",
+  
+    messagingSenderId: "543231526802",
+  
+    appId: "1:543231526802:web:5e50c19b8f861e7918234b"
+  
+  })),
+    provideFirestore(() => getFirestore()),]
 };
